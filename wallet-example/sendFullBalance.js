811175c3a67e13232e349b6b60617ba56a9fc6f0
rileystephens28 <riley.stephens28@gmail.com>
2025-02-17 20:30:14 -0600

    Add example of sending entire Quai balance

---
+++ wallet-example/sendFullBalance.js
@@ -0,0 +1,100 @@
+import * as quais from "quais";
+
+/**
+ * Send Full Quai Balance in one transaction
+ *
+ * This script demonstrates how to send the full balance of a Quai wallet in one transaction.
+ * Generally, quais.js will populate several of the transaction parameters for you, but in this case
+ * we must manually set the gas fields (gasLimit, gasPrice) and use the gas values to determine the
+ * amount of Quai to send.
+ *
+ * The script demonstrates:
+ * 1. Creating and connecting a Wallet to a provider
+ * 2. Getting the current block and protocol expansion number
+ * 3. Getting the next address from the wallet
+ * 4. Signing a transaction
+ * 5. Sending a transaction
+ *
+ * Usage:
+ * First, set up your .env file with:
+ * PRIVATE_KEY="your private key here"
+ * RECIPIENT_ADDRESS="recipient address here"
+ *
+ * Then run:
+ * ```
+ * node sendFullBalance.js
+ * ```
+ *
+ * The script will output:
+ * - The signer address
+ * - The recipient address
+ * - The amount of Quai sent
+ * - The gas parameters used
+ * - The transaction hash
+ *
+ */
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const RECIPIENT_ADDRESS = process.env.RECIPIENT_ADDRESS;
+
+async function sendFullBalance() {
+  // Initialize connection to Quai Network, Quais will route to shards automatically
+  const provider = new quais.JsonRpcProvider(
+    "https://orchard.rpc.quai.network"
+  );
+
+  // Create wallet from private key
+  const wallet = new quais.Wallet(PRIVATE_KEY, provider);
+
+  const balance = await provider.getBalance(wallet.address);
+  console.log("Balance: ", balance);
+
+  // Construct a basic transaction
+  const from = wallet.address;
+  const txForEstimateGas = {
+    to: RECIPIENT_ADDRESS, // Recipient address
+    value: balance / 10n, // Use a small test balance for gas estimation
+    from: from, // Sender address
+  };
+
+  // Estimate the gas for the transaction
+  const estimatedGas = await wallet.estimateGas(txForEstimateGas);
+
+  // Get gas price for the zone
+  const txLocation = quais.getZoneForAddress(from);
+  const { gasPrice } = await provider.getFeeData(txLocation);
+
+  // Calculate the sendable balance
+  const sendableBalance = balance - estimatedGas * gasPrice;
+
+  // Check if the sendable balance is more than cost of the transaction
+  if (sendableBalance <= 0) {
+    console.log("Not enough balance to send");
+    return;
+  } else {
+    console.log("Sendable balance: ", sendableBalance);
+  }
+
+  const txObj = {
+    to: RECIPIENT_ADDRESS, // Recipient address
+    value: sendableBalance, // Send the sendable balance
+    from: from, // Sender address
+  };
+
+  // Send the transaction
+  const txResponse = await wallet.sendTransaction(txObj);
+  console.log("Transaction broadcasted. Waiting for inclusion...");
+
+  // Wait for the transaction to be included in a block
+  const txReceipt = await txResponse.wait();
+  console.log("Transaction included in block ", txReceipt.blockNumber);
+  console.log("Transaction hash: ", txResponse.hash);
+}
+
+// Execute main function with error handling
+sendFullBalance()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
